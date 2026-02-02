7dcda143ecc4eb992a4e7df32cb9412fa01bbd3c
YHyoyeon <gydus.dev@gmail.com>
2026-02-02 15:26:11 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    preserveSymlinks: true,
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
   },
 });
